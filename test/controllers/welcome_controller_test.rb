259fdc5e21da9209acd7a53172701e9f4cb7e042
Taha <taha_016@hotmail.co.uk>
2015-02-25 15:48:17 +0000

    KL

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
+  test "should get test" do
+    get :test
     assert_response :success
   end
 
